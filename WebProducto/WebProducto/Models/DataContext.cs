6a14a44b8712a6bdd2619a696bb931f4a808b97f
victoriosote <a2019112604@estudiantes.upsa.edu.bo>
2021-01-20 21:06:58 -0400

    segundo commit

---
+++ WebProducto/WebProducto/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace WebProducto.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<WebProducto.Models.Product> Products { get; set; }
+    }
+}
---
+++ WebProducto/WebProducto/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿namespace WebProducto.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public enum MarketType 
+    {
+        niños=0,
+        jovenes=1,
+        hombres=2,
+        mujeres=3,
+        mayores=4,
+    }
+    public enum CategoryType
+    {
+        ropa=0,
+        electronicos=1,
+        cosmeticos=2,
+        muebleria=3,
+        juegos=4,
+        deportes=5,
+        bebidas=6,
+        alimentos=7,
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        [StringLength(80,ErrorMessage="Nombre entre 5 y 80 choco",MinimumLength=5)]
+        public string Name { get; set; }
+        [Range(0, 10000, ErrorMessage = "El precio oscila entre 0 a 10000")]
+        public double Price { get; set; }
+        public MarketType Market  { get; set; }
+        public CategoryType Category { get; set; }
+
+    }
+}
